2108637b3ae6c7ecc8e0947d960d0c17429a3c9e
LukasW01 <lukas@wigger.dev>
2024-06-21 23:53:58 +0200

    fix: rubocop

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Lernmit
   class Application < Rails::Application
+    config.assets.css_compressor = nil
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
@@ -15,8 +17,6 @@ module Lernmit
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-    
-    config.assets.css_compressor = nil
 
     # Configuration for the application, engines, and railties goes here.
     #
