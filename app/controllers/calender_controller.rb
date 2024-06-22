66f75562394529e8882acec1f258f266338daa80
LukasW01 <lukas@wigger.dev>
2024-06-22 18:38:05 +0200

    feat(LERNMIT-15): Lernserien & Spezielle Achievements

--- app/controllers/calender_controller.rb
+++ app/controllers/calender_controller.rb
@@ -1,8 +1,37 @@
 class CalenderController < ApplicationController
   before_action :require_login
-  def index; end
 
-  def week; end
+  def index
+    load_tasks('month')
+  end
 
-  def month; end
+  def week
+    load_tasks('week')
+  end
+
+  def month
+    load_tasks('month')
+  end
+
+  private
+
+  def load_tasks(view)
+    @load_tasks ||= task_scope.where(due_date: date_range(view)).order(created_at: :desc).to_a
+  end
+
+  def task_scope
+    Task.where(user_id: current_user.id, types: 'Exam')
+  end
+
+  def date_range(view)
+    if view == 'month'
+      start_date = params[:start_date].present? ? params[:start_date].to_date.beginning_of_month : Date.today.beginning_of_month
+      end_date =  start_date.end_of_month
+    elsif view == 'week'
+      start_date = params[:start_date].present? ? params[:start_date].to_date.beginning_of_week : Date.today.beginning_of_week
+      end_date = start_date.end_of_week
+    end
+
+    start_date..end_date
+  end
 end
